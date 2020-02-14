7bcbdfa4547caa9bb57589fd4bb7acdd5a76e75e
Matthew Nielsen <xunker@pyxidis.org>
2020-02-14 12:51:32 -0700

    working case fade

--- nvme_cooler.ino
+++ nvme_cooler.ino
@@ -9,13 +9,15 @@
 // sk6812<C,6>  strip;
 // sk6812b<C,6>  strip;
 // ws2812<C,6>  strip;
-ws2812b<C,6>  strip;
+// ws2812b<C,6>  strip;
+// apa106<C,6>  strip;
+apa104<C,6>  strip;
 
 // How many pixels to control
 const uint8_t numPixels = 4;
 
 // How bright the LEDs will be (max 255)
-const uint8_t maxBrightness = 127;
+const uint8_t maxBrightness = 63;
 
 // The pixel array to display
 grb  pixels[numPixels] = {};
@@ -28,65 +30,89 @@ void updateColors(char r, char g, char b) {
   }
 }
 
+#define FIRST_PIXEL 0
+uint8_t currentPixel = FIRST_PIXEL;
+
+byte currentBrightness = 1;
+#define BRIGHT_INC 2
+#define BRIGHT_DEC -1
+
+int8_t brightnessDirection = BRIGHT_INC;
+
+int16_t pixelBrightness[numPixels] = {}; // 16-bit int to account for states above maxBrightness or below 0
+int8_t pixelDirection[numPixels] = {};
+
+#define FADE_EVERY 25 // ms
+unsigned long nextFade = FADE_EVERY;
+
+#define MOVE_EVERY 1000 // ms
+unsigned long nextMove = MOVE_EVERY;
+
 void setup() {
   // Serial.begin(57600);
   // Turn off the LEDs
   strip.clear(2 * numPixels);
+
+  for (uint8_t i = 0; i < numPixels; i++)
+  {
+    pixelBrightness[i] = 0;
+    pixelDirection[i] = 0;
+  }
 }
 
-uint8_t currentPixel = -1;
+void chaseFade() {
+  unsigned long currentMillis = millis();
 
-byte currentBrightness = 1;
-#define BRIGHT_INC 5
-#define BRIGHT_DEC -5
+  if (nextFade < currentMillis) {
+    nextFade = currentMillis + FADE_EVERY;
 
-int8_t brightnessDirection = BRIGHT_INC;
+    for(uint8_t i = 0; i < numPixels; i++) {
+      if (pixelDirection[i] == 0)
+        continue;
 
-void chase() {
-  pixels[currentPixel].r = currentBrightness;
-  pixels[currentPixel].g = 0;
-  pixels[currentPixel].b = 0;
+      pixelBrightness[i] = pixelBrightness[i] + pixelDirection[i];
 
-  strip.sendPixels(numPixels, pixels);
+      pixels[i].g = pixelBrightness[i];
 
-  delay(100/4);
+      strip.sendPixels(numPixels, pixels);
 
-  pixels[currentPixel].r = 0;
-  pixels[currentPixel].g = 0;
-  pixels[currentPixel].b = 0;
+      if (pixelBrightness[i] > maxBrightness) {
+        pixelDirection[i] = BRIGHT_DEC;
+      } else if (pixelBrightness[i] < 1) {
+        pixelDirection[i] = 0;
+      }
+    }
+  }
 
-  currentPixel++;
-  if (currentPixel > numPixels)
+  if (nextMove < currentMillis) {
+    nextMove = currentMillis + MOVE_EVERY;
+
+    currentPixel++;
+    if (currentPixel >= numPixels)
+      currentPixel = FIRST_PIXEL;
+
+    pixelDirection[currentPixel] = BRIGHT_INC;
+  }
+}
+
+void simple() {
+  if (currentPixel < 0)
     currentPixel = 0;
 
-  currentBrightness = currentBrightness + brightnessDirection;
-  // Serial.println(currentBrightness);
-  if (currentBrightness > maxBrightness) {
-    // currentBrightness = 1;
-    brightnessDirection = -1;
-  } else if (currentBrightness < 1) {
-    brightnessDirection = 1;
-  }
-}
-
-void pulseAll() {
-  updateColors(currentBrightness, 0, 0);
+  pixels[currentPixel].r = maxBrightness;
 
   strip.sendPixels(numPixels, pixels);
 
-  delay(100/20);
+  pixels[currentPixel].r = 0;
 
-  currentBrightness = currentBrightness + brightnessDirection;
-  // Serial.println(currentBrightness);
-  if (currentBrightness > maxBrightness) {
-    // currentBrightness = 1;
-    brightnessDirection = BRIGHT_DEC;
-  } else if (currentBrightness < 1) {
-    brightnessDirection = BRIGHT_INC;
-  }
+  delay(1000);
+  currentPixel++;
+  if (currentPixel > numPixels-1)
+    currentPixel = 0;
+
 }
 
 void loop() {
-  chase();
-  // pulseAll();
+  chaseFade();
+  // simple();
 }
